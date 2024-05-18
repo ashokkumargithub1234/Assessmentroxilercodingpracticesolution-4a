78bc6066164d02fee132a87ada8eff689cd302f5
ashokkumargithub1234 <ashokkumarpenjuvvi@gmail.com>
2024-05-18 16:56:12 +0000

    first commit

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const format = require("date-fns/format");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const databasePath = path.join(__dirname, "productsTransaction.db");
 
